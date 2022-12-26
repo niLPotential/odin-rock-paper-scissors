b5e19a82ddc8a83d438c1c8c2cf0f860d29d73c2
niLPotential <niLPotential@proton.me>
2022-12-26 19:47:12 +0900

    Renew getChoice functions Implement playRound

--- script.js
+++ script.js
@@ -1,24 +1,17 @@
 function getComputerChoice() {
-  const randomNumber = Math.random() * 3;
-
-  if (randomNumber < 1) {
-    return "Rock";
-  } else if (randomNumber < 2) {
-    return "Paper";
-  } else if (randomNumber < 3) {
-    return "Scissors";
-  }
+  // 0 is rock, 1 is paper, 2 is scissors
+  return Math.floor(randomNumber = Math.random() * 3);
 }
 
 function getPlayerChoice() {
   let input = prompt("Rock, Paper, Scissors!");
   switch (input.toLowerCase()) {
     case "rock":
-      return "Rock";
+      return 0;
     case "paper":
-      return "Paper";
+      return 1;
     case "scissors":
-      return "Scissors";
+      return 2;
     default:
       console.log("Enter a valid input");
       return getPlayerChoice();
@@ -26,4 +19,18 @@ function getPlayerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+  switch (playerSelection - computerSelection) {
+    case 0:
+      return `It's a Draw! Both played ${playerSelection}`;
+    case 1:
+    case -2:
+      return `You win! ${playerSelection} beats ${computerSelection}`;
+    case 2:
+    case -1:
+      return `You lose! ${playerSelection} loses to ${computerSelection}`;
+    default:
+      return `Sorry, something went wrong: ${playerSelection} ${computerSelection}`;
+  }
 }
+
+console.log(playRound(getPlayerChoice(), getComputerChoice()));
